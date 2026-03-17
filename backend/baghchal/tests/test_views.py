44660beebf69aece11378cab58e3a796764f1eeb
Tailung42 <tailung42@gmail.com>
2026-03-17 23:35:23 +0545

    chore: write tests for quick-match view

---
+++ backend/baghchal/tests/test_views.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from unittest.mock import patch
+from core.models import User
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+class QuickMatchTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        super().setUpClass()
+        cls.user = User.objects.create_user(username="testuser", password="testpass")
+
+    @patch("baghchal.views.get_all_games", return_value={"0000": {"status": "waiting", "player": {"goat": "", "tiger": "hero"}}})
+    def test_quick_match_with_existing_game(self, *_):
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+        # url = reverse("game/quick-match/")
+        response = self.client.post("/game/quick-match/")
+        self.assertEqual(response.data,{"game_id": "0000"})
+
+    @patch("baghchal.views.get_all_games", return_value={"1111": {"status": "ongoing"}})
+    @patch("baghchal.views.uuid.uuid4", return_value="0000")
+    def test_quick_match_without_existing_game(self, *_):
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+        response = self.client.post("/game/quick-match/")
+        self.assertEqual(response.data["game_id"], "0000")
