aac957622c866160b026bb67a538f160bbdede81
Tailung42 <tailung42@gmail.com>
2026-03-17 22:17:23 +0545

    refactor: update redis file to connect using tcp connection
    redis.from_url() allows connection connection using tcp connection

--- backend/baghchal/redis.py
+++ backend/baghchal/redis.py
@@ -5,17 +5,10 @@ import json
 import redis
 import os
 
-# Initialize Redis client
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
-REDIS_DB = int(os.environ.get('REDIS_DB', 0))
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    db=REDIS_DB,
-    decode_responses=True
-)
+# Initialize Redis client
+redis_client = redis.from_url(REDIS_URL)
 
 # Key prefix for all game states
 GAME_KEY_PREFIX = "game:"
