d56b9613623b1f7139da9ab366fb9ea4ed494889
Tailung42 <tailung42@gmail.com>
2026-03-29 21:32:14 +0545

    test: use async repository functions for redis

--- backend/baghchal/tests/test_views.py
+++ backend/baghchal/tests/test_views.py
@@ -12,7 +12,7 @@ class QuickMatchTestCase(TestCase):
         super().setUpClass()
         cls.user = User.objects.create_user(username="testuser", password="testpass")
 
-    @patch("baghchal.views.get_all_games", return_value={"0000": {"status": "waiting", "player": {"goat": "", "tiger": "hero"}}})
+    @patch("baghchal.views.async_get_all_games", return_value={"0000": {"status": "waiting", "player": {"goat": "", "tiger": "hero"}}})
     def test_quick_match_with_existing_game(self, *_):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
@@ -21,7 +21,7 @@ class QuickMatchTestCase(TestCase):
         response = self.client.post("/game/quick-match/")
         self.assertEqual(response.data,{"game_id": "0000"})
 
-    @patch("baghchal.views.get_all_games", return_value={"1111": {"status": "ongoing"}})
+    @patch("baghchal.views.async_get_all_games", return_value={"1111": {"status": "ongoing"}})
     @patch("baghchal.views.uuid.uuid4", return_value="0000")
     def test_quick_match_without_existing_game(self, *_):
         self.client = APIClient()
